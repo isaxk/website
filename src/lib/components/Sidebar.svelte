5d1c535e4c9dea042cc3f4af1e949d262305ab76
isaacboor <iceboorman@gmail.com>
2023-12-23 16:57:15 +0000

    adjustments

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -5,8 +5,11 @@
 				<img src="/profilepicture.jpg" alt="Profile" />
 			</div>
 			<div class="text">
-				<div class="name">isaxk (Isaac)</div>
+				<div class="name">Isaac <span class="username">(isaxk)</span></div>
 				<div class="bio">Casual Programmer</div>
+			</div>
+			<div class="social">
+				<div class="github"><a href="https://github.com/isaxk"><i class="fa-brands fa-github"></i></a></div>
 			</div>
 		</div>
 		<div class="links">
@@ -17,22 +20,26 @@
 
 <style lang="scss">
 	.sidebar-container {
-		padding: 100px 50px 30px 0px;
+		padding: 100px 10px 30px 0px;
         height: max-content;
 	}
     .sidebar {
         padding-bottom: 30px;
     }
-	@media screen and (max-width: 680px) {
+	@media screen and (max-width: 850px) {
 		.sidebar-container {
-			padding: 10px 50px 10px 0px;
+			padding: 10px 10px 10px 0px;
 			border: #ffffffd1 0px solid;
 			border-bottom-width: 2px;
 		}
 	}
+
+	.social {
+		font-size: 30px;
+	}
 	.profile {
 		display: grid;
-		grid-template-columns: 75px max-content;
+		grid-template-columns: 75px 1fr min-content;
 		align-items: center;
 		height: 100px;
 		gap: 1.5rem;
@@ -58,5 +65,12 @@
 	.links {
 		margin: 25px 0px 0px 0px;
 		padding: 0px 20px;
+	}
+	.username {
+		font-weight: 300;
+		color: #ffffffd1;
+	}
+	.name {
+		font-weight: 800;
 	}
 </style>
