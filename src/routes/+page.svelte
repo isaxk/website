6b3ceb48fb853bfc82aa067fdb990c43803eedff
isaacboor <iceboorman@gmail.com>
2024-02-11 09:32:44 +0000

    Initial Commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,2 @@
-<svelte:head><title>Home - isaxk.com</title></svelte:head>
-
-<h1>Welcome</h1>
-<p>Hey I'm Isaac, a casual programmer who makes a few cool projects from time to time. This website is just a place for me to keep my projects.<br><a href="/about">More...</a></p>
-
-<h2>Featured Projects:</h2>
-<ul>
-	<li><a href="https://flags.isaxk.com">QuickFlags</a></li>
-	<li><a href="https://weirdtext.isaxk.com">WeirdText</a></li>
-</ul>
-<a href="/projects">All projects</a>
-
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
