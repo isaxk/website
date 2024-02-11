fda5703780b5b38c057569826fb90255919d4c0e
isaacboor <iceboorman@gmail.com>
2024-02-11 13:39:16 +0000

    new design

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,83 @@
+<script>
+	import SocialIcon from "./SocialIcon.svelte";
+
+</script>
+<div class="hero">
+    <div class="top-row">
+        
+        <div class="left">
+            <div class="greeting">
+                Hi! 
+            </div>
+            <div class="title">
+                I'm Isaac <div class="light">(@isaxk)</div>
+            </div>
+            <div class="bio">Casual Web Developer</div>
+            <div class="socials">
+                <SocialIcon icon="github" href="https://github.com/isaxk"/>
+                <SocialIcon icon="discord" href="https://discordapp.com/users/988006561442041886"/>
+            </div>
+        </div>
+        <div class="profile-picture">
+            <img src="/profilepicture.jpg" alt="profile">
+        </div>
+    </div>
+
+</div>
+
+<style>
+    .hero {
+        max-width: 640px;
+        width: 100%;
+        padding: 0px 20px;
+        box-sizing: border-box;
+    }
+    .top-row {
+        display: grid;
+        grid-template-columns: 2fr 1fr;
+        gap: 20px;
+        align-items: center;
+    }
+    @media screen and (max-width: 530px) {
+        .top-row {
+            grid-template-columns: 1fr;
+            place-items: center;
+            text-align: center;
+            gap: 0px;
+            padding-bottom: 50px;
+        }
+    }
+    .left {
+        display: grid;
+        grid-template-rows: 1fr 1fr 1fr 1fr;
+        gap: 0px;
+        align-items: center;
+        padding: 20px 0px;
+    }
+
+    .greeting {
+        font-size: 25px;
+    }
+    .greeting, .bio, .light {
+        color: #ffffffd0;
+    }
+    .title {
+        font-size: 35px;
+        font-weight: 800;
+    }
+    .light {
+        font-weight: 300;
+        font-size: 80%;
+        transform: translateY(-2.5px);
+        display: inline-block;
+    }
+    .profile-picture {
+        width: 100%;
+        max-width: 180px;
+    }
+    .profile-picture img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,81 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Hero from "$lib/components/Hero.svelte";
+</script>
+
+<main>
+	<div class="hero-container">
+		<Hero />
+	</div>
+	<div class="content-container">
+		<div class="content">
+			<div class="section">
+				<h2>About</h2>
+				<p>
+					I'm Isaac a casual web developer from the UK. My main area of
+					knowledge is in Javascript, TypeScript, CSS and Svelte.
+				</p>
+			</div>
+
+			<div class="section">
+				<h2>Projects</h2>
+				<p class="projects">
+					<a href="https://flags.isaxk.com">QuickFlags</a>
+					<a href="https://weirdtext.isaxk.com">WeirdText</a>
+				</p>
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	.hero-container {
+		height: 60vh;
+		min-height: 400px;
+		max-height: 600px;
+		display: grid;
+		place-items: center;
+		background-image: linear-gradient(-45deg, #32324a, #294253, #20204f);
+		background-attachment: fixed;
+		background-size: 400% 400%;
+		animation: AnimatedBg 30s ease infinite;
+	}
+
+	@keyframes AnimatedBg {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
+	.content-container {
+		width: 100%;
+		display: grid;
+		justify-items: center;
+		min-height: 40vh;
+		padding: 40px 20px;
+		box-sizing: border-box;
+	}
+
+	.content {
+		max-width: 600px;
+		width: 100%;
+	}
+	.section {
+		margin: 50px 0px 70px 0px;
+	}
+
+	.projects a {
+		color: white;
+		font-size: 18px;
+		text-decoration: none;
+		display: inline-block;
+		background-color: #293b4c9d;
+		padding: 10px 20px;
+		border-radius: 5px;
+		margin-right: 5px;
+	}
+</style>
