b72ee40f80232eb151d1056244ed336bd197f162
Isaac <iceboorman@gmail.com>
2024-02-21 21:00:44 +0000

    clean ups ig

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -1,107 +1,101 @@
 <script lang="ts">
-	import { page } from "$app/stores";
+    import { page } from "$app/stores";
 
     import { crossfade, slide } from "svelte/transition";
 
-    import { ripple } from "svelte-ripple-action";
+    const [send, receive] = crossfade({
+        duration: 300,
+    });
 
-	const [send, receive] = crossfade({
-		duration: 300,
-	});
+    const [sendMenu, receiveMenu] = crossfade({
+        duration: 300,
+    });
 
-	const [sendMenu, receiveMenu] = crossfade({
-		duration: 300,
-	});
-	const menuItems = [
+    const menuItems = [
         {
             text: "Home",
-            href: "/"
+            href: "/",
         },
-		{
-			text: "About",
-			href: "/about",
-		},
-		{
-			text: "Projects",
-			href: "/projects",
-		},
-
-	];
+        {
+            text: "About",
+            href: "/about",
+        },
+        {
+            text: "Projects",
+            href: "/projects",
+        },
+    ];
 </script>
 
 <header>
-	<nav>
-		{#each menuItems as item}
-			<div class="item">
-				{#if $page.url.pathname === item.href}
-					<a href={item.href} class="active-page">{item.text}</a>
-				{:else}
-					<a href={item.href}>{item.text}</a>
-				{/if}
+    <nav>
+        {#each menuItems as item}
+            <div class="item">
+                {#if $page.url.pathname === item.href}
+                    <a href={item.href} class="active-page">{item.text}</a>
+                {:else}
+                    <a href={item.href}>{item.text}</a>
+                {/if}
 
-				{#if $page.url.pathname === item.href}
-					<div
-						class="underline"
-						in:receive={{ key: "menu" }}
-						out:send={{ key: "menu" }}
-					></div>
-<!-- 				{:else}
+                {#if $page.url.pathname === item.href}
+                    <div
+                        class="underline"
+                        in:receive={{ key: "menu" }}
+                        out:send={{ key: "menu" }}
+                    ></div>
+                    <!-- 				{:else}
 					<div
 						class="no-underline"
 						in:receive={{ key: "menu" }}
 						out:send={{ key: "menu" }}
 					></div> -->
-				{/if}
-			</div>
-		{/each}
-	</nav>
+                {/if}
+            </div>
+        {/each}
+    </nav>
 </header>
 
 <style>
-	header {
-		position: sticky;
-		top: 0;
+    header {
+        position: sticky;
+        top: 0;
         max-width: 600px;
         margin: auto;
-	}
-	nav {
+    }
+    nav {
         width: max-content;
         margin: auto;
-		padding: 10px 0 0 0;
-		box-sizing: border-box;
-	}
-	nav .item {
-		display: inline-block;
-		padding: 10px 5px;
+        padding: 10px 0 0 0;
+        box-sizing: border-box;
+    }
+    nav .item {
+        display: inline-block;
+        padding: 10px 5px;
         height: 20px;
         border-bottom: 3px solid #ffffff12;
-	}
-	nav a {
+    }
+    nav a {
         display: block;
-		color: #d3d3d3ea;
-		text-decoration: none;
-		margin: 0px 5px;
-		transition: 0.5s;
+        color: #d3d3d3ea;
+        text-decoration: none;
+        margin: 0px 5px;
+        transition: 0.5s;
         padding-bottom: 5px;
         border-bottom-width: 2px;
         border: transparent 2px solid;
         border-radius: 2px;
-	}
+    }
     nav a:hover {
         transition: 0.5s;
         color: #fffffff3;
     }
-	nav a.active-page {
-		color: white;
-	}
-	.underline {
-		background-color: #d3d3d3;
+    nav a.active-page {
+        color: white;
+    }
+    .underline {
+        background-color: #d3d3d3;
         margin-top: 0px;
-		height: 2px;
-		border-radius: 5px;
-	}
-    .no-underline {
-        background-color: transparent;
         height: 2px;
+        border-radius: 5px;
     }
 </style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,84 +1,89 @@
 <script lang="ts">
-	const projects = [
-		{
-			name: "WeirdText",
-			description: "Changes your text in wierd and interesting ways",
-			link: "https://weirdtext.isaxk.com",
+    const projects = [
+        {
+            name: "WeirdText",
+            description: "Changes your text in wierd and interesting ways",
+            link: "https://weirdtext.isaxk.com",
             background: "/WeirdText.jpeg",
-		},
+        },
         {
-			name: "QuickFlags",
-			description: "The fast paced flag naming game",
-			link: "https://flags.isaxk.com",
+            name: "QuickFlags",
+            description: "The fast paced flag naming game",
+            link: "https://flags.isaxk.com",
             background: "/QuickFlags.jpeg",
-		},
+        },
         {
-			name: "Since this page loaded",
-			description: "Counts what has happenned around the world.",
-			link: "https://sincethispageloaded.isaxk.com",
+            name: "Since this page loaded",
+            description: "Counts what has happenned around the world.",
+            link: "https://sincethispageloaded.isaxk.com",
             background: "/sincethispageloaded.png",
-		},
-	];
+        },
+    ];
 </script>
 
 <svelte:head>
-	<title>isaxk - Projects</title>
+    <title>isaxk - Projects</title>
 </svelte:head>
 
 <h1>Projects</h1>
 
 <div class="projects-grid">
-	{#each projects as project}
-		<div class="project" style="background-image: url('{project.background}');">
-			<a class="info-container" href={project.link}>
-				<div class="info-content">
-					<div class="name">{project.name}</div>
-					<div class="description">{project.description}</div>
-					<div class="link"><a href={project.link}>Take a look</a></div>
-				</div>
-			</a>
-		</div>
-	{/each}
+    {#each projects as project}
+        <div
+            class="project"
+            style="background-image: url('{project.background}');"
+        >
+            <a class="info-container" href={project.link}>
+                <div class="info-content">
+                    <div class="name">{project.name}</div>
+                    <div class="description">{project.description}</div>
+                    <div class="link">
+                        <a href={project.link}>Take a look</a>
+                    </div>
+                </div>
+            </a>
+        </div>
+    {/each}
 </div>
 
 <style>
-	.projects-grid {
-		display: grid;
-		grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+    .projects-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
         gap: 20px;
-	}
-	.project {
-		background-color: #121212;
-		border-radius: 5px;
-		position: relative;
-		background-size: 100%;
+    }
+    .project {
+        background-color: #121212;
+        border-radius: 5px;
+        position: relative;
+        background-size: 100%;
         background-position: center top;
-		height: 200px;
+        height: 200px;
         transition: 0.2s ease-in-out;
-	}
+    }
     .project:hover {
         background-size: 105%;
     }
-	img {
-		width: 100%;
-		object-fit: contain;
-		border-radius: 5px;
-	}
-	.info-container {
-		position: absolute;
-		bottom: 0;
-		left: 0;
-		width: 100%;
-		height: 100%;
-		padding: 15px;
-		box-sizing: border-box;
-		background-image: linear-gradient(to bottom, transparent, #121212b9, #090909);
-		border-radius: 5px;
+    .info-container {
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        padding: 15px;
+        box-sizing: border-box;
+        background-image: linear-gradient(
+            to bottom,
+            transparent,
+            #121212b9,
+            #090909
+        );
+        border-radius: 5px;
         display: grid;
         align-items: end;
         color: white;
         text-decoration: none;
-	}
+    }
     .name {
         font-size: 18px;
         font-weight: 500;
